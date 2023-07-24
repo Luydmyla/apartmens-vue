ecc3d7f785002a70412d519f04568d130faadcee
Luydmyla <lytic.ok@gmail.com>
2023-07-25 01:45:29 +0300

    added scss, styles in CustomInput

--- src/components/shared/CustomInput.vue
+++ src/components/shared/CustomInput.vue
@@ -1,5 +1,5 @@
 <template>
-  <input :value="modelValue" @input="onInput" />
+  <input :value="modelValue" @input="onInput" class="custom-input" />
 </template>
 
 <script>
@@ -19,4 +19,21 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import "../../assets/scss/variables";
+
+.custom-input {
+  height: 40px;
+  max-width: 220px;
+  width: 100%;
+  border: 2px solid $main-color;
+  font-size: 18px;
+  outline: none;
+  line-height: inherit;
+  padding: 8px 15px;
+
+  &::placeholder {
+    color: inherit;
+  }
+}
+</style>
