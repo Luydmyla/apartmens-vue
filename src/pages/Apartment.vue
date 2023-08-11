b8f0cf61f84de3b2f3280c12b90e3c8667749cb9
Luydmyla <lytic.ok@gmail.com>
2023-08-11 17:30:32 +0300

    added  ApartmentsOwner

--- src/pages/Apartment.vue
+++ src/pages/Apartment.vue
@@ -1,8 +1,14 @@
 <template>
   <main class="apartment-page">
     <Container>
-      <!-- <h1>Apartment Page</h1> -->
-      <ApartmentsMainInfo :apartment="apartment" />
+      <div class="apartment-page__content">
+        <!-- <h1>Apartment Page</h1> -->
+        <ApartmentsMainInfo :apartment="apartment" />
+        <ApartmentsOwner
+          :owner="apartment.owner"
+          class="apartment-page__owner"
+        />
+      </div>
     </Container>
   </main>
 </template>
@@ -10,10 +16,12 @@
 <script>
 import apartments from "../components/apartment/apartments.js";
 import ApartmentsMainInfo from "../components/apartment/ApartmentsMainInfo.vue";
+import ApartmentsOwner from "../components/apartment/ApartmentsOwner.vue";
+
 import Container from "../components/shared/Container.vue";
 export default {
   name: "ApartmenPage",
-  components: { Container, ApartmentsMainInfo },
+  components: { Container, ApartmentsMainInfo, ApartmentsOwner },
   computed: {
     apartment() {
       return apartments.find(
@@ -34,5 +42,13 @@ export default {
 <style lang="scss" scoped>
 .apartment-page {
   padding-bottom: 55px;
+  &__content {
+    display: flex;
+    align-items: flex-start;
+  }
+  &__owner {
+    min-width: 350px;
+    margin-left: 30px;
+  }
 }
 </style>
