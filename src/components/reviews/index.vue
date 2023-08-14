1ec159371aaa3913d2921e0ea6926958e3c50ff7
Luydmyla <lytic.ok@gmail.com>
2023-08-15 01:53:35 +0300

    added total reviews

--- src/components/reviews/index.vue
+++ src/components/reviews/index.vue
@@ -3,8 +3,8 @@
     <div class="reviews__heading">
       <h2 class="reviews__title">Сумарний рейтинг</h2>
       <div class="reviews__rating">
-        <span>98 відгуків</span>
-        <Rating :rating="0" />
+        <span>{{ amountOfReviews }} відгук(ів)</span>
+        <Rating :rating="totalRating" />
       </div>
     </div>
     <ReviewsItem
@@ -32,15 +32,46 @@ export default {
       required: true,
     },
   },
+  computed: {
+    totalRating() {
+      const total = this.reviews.reduce(
+        (acc, review) => acc + review.rating,
+        0
+      );
+      return total / this.reviews.length;
+    },
+    amountOfReviews() {
+      return this.reviews.length;
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
+@import "../../assets/scss/variables";
 .reviews {
   margin-top: 20px;
-  /* background: $card-bg; */
-  background: blue;
+  background: $card-bg;
+  /* background: blue; */
   padding: 0 2px;
+
+  &__heading {
+    padding: 20px 20px 10px;
+  }
+
+  &__title {
+    text-transform: uppercase;
+    font-size: 20px;
+    font-weight: 700;
+    margin-bottom: 10px;
+  }
+
+  &__rating {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
   &__show-more {
     background: none;
     border: none;
