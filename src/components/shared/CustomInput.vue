fa5b434cd63926a0e174ef93883a4b2d64a48216
Luydmyla <lytic.ok@gmail.com>
2023-08-20 16:05:32 +0300

    added validation to input

--- src/components/shared/CustomInput.vue
+++ src/components/shared/CustomInput.vue
@@ -1,20 +1,47 @@
 <template>
-  <input :value="modelValue" @input="onInput" class="custom-input" />
+  <div class="wrapper-input">
+   <input :value="modelValue" @input="onInput" class="custom-input" />
+   <span v-if="!isValid" class="custom-input__error">{{ errorMessage }}</span>
+  </div>
 </template>
 
 <script>
 export default {
   name: "CustomInput",
+  data() {
+    return {
+      isValid: true
+    }
+  },
   props: {
     modelValue: {
       type: String,
       required: true,
     },
+
+    errorMessage: {
+      type: String,
+      default: ""
+    },
+    rules: {
+      type: Array,
+      // якщо ми вказуємо дефолтне значення ссилочного типу, то робимо це через функцію
+      default: () => []
+    }
   },
+watch: {
+  modelValue(value){
+    this.validate(value)
+    console.log(value)
+  }
+},
   methods: {
     onInput(event) {
       this.$emit("update:modelValue", event.target.value);
     },
+    validate(value) {
+      this.isValid = this.rules.every((rule) => rule(value))
+    }
   },
 };
 </script>
